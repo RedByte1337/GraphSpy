5f9dc7d0ed0792ba5089ba2ec08cef730e997361
RedByte1337 <keanu.nys@hotmail.com>
2024-03-10 11:42:04 +0100

    Use default values for MS Graph and Microsoft Office as resource and client ID when generating a device code

--- GraphSpy/GraphSpy.py
+++ GraphSpy/GraphSpy.py
@@ -342,10 +342,12 @@ def init_routes():
 
     @app.post('/api/generate_device_code')
     def api_generate_device_code():
-        resource = request.form['resource'] if "resource" in request.form else ""
-        client_id = request.form['client_id'] if "client_id" in request.form else ""
+        resource = request.form['resource'] if "resource" in request.form else "https://graph.microsoft.com"
+        client_id = request.form['client_id'] if "client_id" in request.form else "d3590ed6-52b3-4102-aeff-aad2292ab01c"
         if resource and client_id:
             user_code = device_code_flow(resource, client_id)
+        else:
+            user_code = "000000000"
         return user_code
 
     @app.route("/api/delete_device_code/<id>")
